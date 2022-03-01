2706dcb4004797e19ee26b09ed48308e45b030d5
Luke <80142097+LukeRykta@users.noreply.github.com>
2022-03-01 13:48:53 -0600

    added openfile to parse data from txt files

--- Main.java
+++ Main.java
@@ -1,6 +1,39 @@
-public class Main {
-    public static void main(String [] args) {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
-        System.out.println("shut ur ass up Luke!");
+public class Main {
+
+    public static List<String> OpenFile(String input) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(String.format("inputs/%s", input)));
+        List<String> form = new ArrayList<>();
+
+        String line = reader.readLine();
+
+        while (line != null) {
+            form.add(line);
+            line = reader.readLine();
+        }
+
+        reader.close();
+
+        System.out.println(form);
+        System.out.println("----- FINISHED READING FILE -----");
+
+        return form;
+    }
+
+    public static void main(String [] args) throws IOException {
+        int a, i, j;
+        String[] inputs = {"bitcoin.txt", "gamestop.txt", "smallInput.txt"};
+        
+        List<String> bitcoin, gamestop, smallInput;
+        bitcoin = OpenFile(inputs[0]);
+        gamestop = OpenFile(inputs[1]);
+        smallInput = OpenFile(inputs[2]);
+        
+        //System.out.println(Arrays.toString(samples));
     }
 }
