91f90882c6f853302d0add5320ee4b7420b45285
Brandon <brandon_stevenson01@hotmail.com>
2022-03-01 16:00:13 -0600

    STILL NEED TO RETURN PROFIT PROPERLY.

--- Main.java
+++ Main.java
@@ -1,13 +1,36 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
+    private final int count = 0;
+
+    public static double getMin(double[] t, int index, int len){
+        double profit = 0;
+        double temp = t[index] - t[index + 1];
+
+        // Uses Math.profit to find the lowest value between current index and next index
+        if(index >= len - 2){
+            return Math.max(t[index] - t[index + 1], profit);
+        }
+
+        for(int i = index + 1; i < t.length; i++){
+            if((t[i] - t[index]) > temp){
+                profit = t[i] - t[index];
+                temp = t[i] - t[index];
+            }
+            else
+                profit = temp;
+        }
+        // This is the logic we'll need to find the profit number
+        // iterates by one through the given array
+        profit = getMin(t, index + 1, len);
+
+        return profit;
+    }
 
     public static void main(String [] args) throws IOException {
         int a, z, j;
+        double optimal;
         double[] bitcoin, gamestop, smallInput;
 
         //names of text files to parse data from
@@ -31,8 +54,11 @@ public class Main {
         gamestop = toDouble.convert(gamestopStr.size(), gamestopStr);
         smallInput = toDouble.convert(smallInputStr.size(), smallInputStr);
 
-        System.out.println("DOUBLE ARRAY 1:");
-        for (i = 0; i < bitcoinStr.size(); i++) {
+
+        optimal = getMin(smallInput, 0, smallInput.length);
+        System.out.println("FINDING OPTIMAL FOR SMALL INPUT ARRAY: " + optimal);
+        //Prints the numbers in double format
+       /* for (i = 0; i < bitcoinStr.size(); i++) {
             System.out.print(bitcoin[i] + " ");
         }
 
@@ -46,7 +72,7 @@ public class Main {
         System.out.println("DOUBLE ARRAY 3:");
         for (i = 0; i < smallInputStr.size(); i++) {
             System.out.print(smallInput[i] + " ");
-        }
-        System.out.println("");
+        } */
+        System.out.println();
     }
 }
