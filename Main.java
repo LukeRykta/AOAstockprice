902095243f76e46ef870ea7c618db6132d60fcdb
Brandon <brandon_stevenson01@hotmail.com>
2022-02-28 18:02:22 -0600

    Changed mean words.

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String [] args) {
 
-        System.out.println("shut ur ass up brandone");
+        System.out.println("shut ur ass up Luke!");
     }
 }
